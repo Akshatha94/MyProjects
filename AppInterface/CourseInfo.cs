5cb4a24e16add3264256eb2bed64b0b176cba6aa
Akshatha94 <akshathabhat866@gmail.com>
2019-08-19 22:53:46 +0200

    Add files via upload

---
+++ AppInterface/CourseInfo.cs
@@ -0,0 +1,18 @@
+﻿using ChamaAssignment.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppInterface
+{
+    /// <summary>
+    /// Holds teacher and current number of students information
+    /// </summary>
+    public class CourseInfo : StudentAgeReport
+    {
+        public string Teacher { get; set; }
+        public List<Student> StudentCollection { get; set; }
+    }
+}
---
+++ AppInterface/StudentAgeReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppInterface
+{
+    /// <summary>
+    /// Holds minimun, maximum and average age of the students for a course plus course total capacity and
+    /// current number of students
+    /// </summary>
+    public class StudentAgeReport
+    {
+        public string Id { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+        public double AvgAge { get; set; }
+        public int Capacity { get; set; }
+        public int Current { get; set; }
+    }
+}
