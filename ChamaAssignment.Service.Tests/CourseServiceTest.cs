8cdf9107258fb2349cbc7289f620136d1f0abe30
Akshatha94 <akshathabhat866@gmail.com>
2019-08-19 23:07:36 +0200

    Add files via upload

---
+++ ChamaAssignment.Service.Tests/CourseServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChamaAssignment.Service.Service;
+using ChamaAssignment.Service.Service.Interfaces;
+using ChamaAssignment.Data;
+
+namespace ChamaAssignment.Service.Tests
+{
+    [TestClass]
+    public class CourseServiceTest
+    {
+        [TestMethod]
+        public void TestGetStudentAgeReport()
+        {
+            //Arrange
+            //Mock DbContext object
+            //ICourseService myCourseService = new CourseService(myDbContext);
+
+            //Act
+            //myCourseService.GetStudentAgeReport();
+
+            //Assert
+            //Check the retuned list for count and other particular values
+        }
+    }
+}
