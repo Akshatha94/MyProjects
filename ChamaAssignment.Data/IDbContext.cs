584ce39e61537e564cedfec224cbcdda9b17c5ee
Akshatha94 <akshathabhat866@gmail.com>
2019-08-19 22:56:19 +0200

    Add files via upload

---
+++ ChamaAssignment.Data/IDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamaAssignment.Data
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<Course> Courses { get; set; }
+        DbSet<Student> Students { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
